988b18702ead090a3677fde15aac9af49958c56d
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-07-03 15:41:46 +0200

    Update 3.7.

--- Verkaufsdaten-Dashboard Homepage/barchart.js
+++ Verkaufsdaten-Dashboard Homepage/barchart.js
@@ -11,10 +11,10 @@ async function createChart() {
   const data = {
     labels: labels,
     datasets: [{
-      label: 'Top 3 Schrauben nach VerkaufteMenge',
+      label: 'Top 3 Schrauben nach Verkaufte Menge',
       data: datasetData,
-      backgroundColor: 'rgba(54, 162, 235, 0.2)',
-      borderColor: 'rgba(54, 162, 235, 1)',
+      backgroundColor: ['rgba(54, 162, 235, 1)', 'rgba(255, 99, 132, 1)', 'rgba(75, 192, 192, 1)'],
+      borderColor: ['rgba(54, 162, 235, 1)', 'rgba(255, 99, 132, 1)', 'rgba(75, 192, 192, 1)'],
       borderWidth: 1
     }]
   };
